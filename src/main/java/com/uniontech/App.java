c857b0dd7bb5427eb00f03182b545ce446161afc
Rave7043 <ravekumar419@gmail.com>
2024-10-11 20:33:31 +0530

    Update App.java

--- src/main/java/com/uniontech/App.java
+++ src/main/java/com/uniontech/App.java
@@ -6,5 +6,6 @@ package com.uniontech;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World! to java");
     }
 }
